3eccd8bbd1719282ee7d8508b1bacc5b401397ff
MistlyH <craftyminer22@gmail.com>
2025-01-27 19:30:09 -0600

    Moner

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,12 @@
-
+public class BankAccount{
+    public static void main(String[] args){
+        float balance = 2175.37f;
+        balance -= 302.50f;
+        balance += 50.03f;
+        balance = balance/2 + 20f;
+        balance -= 1f;
+        balance *= 2f;
+        balance += 1f;
+        System.out.println("$" + balance); // $1961.9001 grrr floating points
+    }
+}
